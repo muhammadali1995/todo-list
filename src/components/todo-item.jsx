c492e08627b65fb8917836b101ee7a6e2e43de40
muhammadali1995 <4377433mtDEV>
2023-01-10 17:52:45 +0500

    style updates

--- src/components/todo-item.jsx
+++ src/components/todo-item.jsx
@@ -10,12 +10,12 @@ export default function TodoItem({ todo }) {
     }
 
 
-    return <label className={`${todo.status === todoStatus.COMPLETE ? 'line-through text-gray-500 border-gray-600' : 'text-blue-500  border-blue-600'} border flex cursor-pointer items-center py-2 px-6 w-80 rounded-lg mt-5`}>
+    return <label className={`${todo.status === todoStatus.COMPLETE ? 'line-through border-gray-600 italic text-gray-500' : 'text-blue-500  border-blue-600'} border flex cursor-pointer items-center py-2 px-6 w-80 rounded-lg mt-5`}>
         <input type="checkbox"
             defaultChecked={todo.status === todoStatus.COMPLETE}
             disabled={todo.status === todoStatus.COMPLETE ? "disabled" : ""}
             id={todo.id} onChange={onComplete} />
-        <p className="ml-2 font-medium text-blue-500 w-full">{todo.text}</p>
+        <p className="ml-2 font-medium w-full">{todo.text}</p>
     </label>
 
 }
